4f386698343923c87c32f1263c599d80d2d4c2ce
Arup paul <aruppaulctg@gmail.com>
2021-06-08 12:44:31 +0600

    sidebar ui design

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import './_sidebar.scss'
+import {
+    MdExitToApp,
+    MdHistory,
+    MdHome,
+    MdLibraryBooks,
+    MdSentimentDissatisfied,
+    MdSubscriptions,
+    MdThumbUp
+} from "react-icons/md";
 function Sidebar() {
     return (
-        <div className="border border-danger">
-            sidebar
-        </div>
+        <nav className="border border-danger sidebar">
+            <li>
+                <MdHome size={23} />
+                <span>Home</span>
+            </li>
+            <li>
+                <MdSubscriptions size={23} />
+                <span>Subscriptions</span>
+            </li>
+            <li>
+                <MdThumbUp size={23} />
+                <span>Like</span>
+            </li>
+            <li>
+                <MdHistory size={23} />
+                <span>History</span>
+            </li>
+
+            <li>
+                <MdLibraryBooks size={23} />
+                <span>Library</span>
+            </li>
+            <li>
+                <MdSentimentDissatisfied size={23} />
+                <span>I don't Know</span>
+            </li>
+            <hr/>
+            <li>
+                <MdExitToApp size={23} />
+                <span>Log Out</span>
+            </li>
+
+             <hr/>
+
+        </nav>
     );
 }
 
