9f3e292e9d8cd8038c138b046ca829cce95103ad
Arup paul <aruppaulctg@gmail.com>
2021-06-08 16:28:24 +0600

    redux setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './_base.scss'
+import { Provider } from "react-redux";
+import store from './redux/store'
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,
     document.getElementById('root')
 );
 
