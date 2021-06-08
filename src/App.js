87a82b2100eecbdef149f4e7cc24252185bcb275
Arup paul <aruppaulctg@gmail.com>
2021-06-08 15:59:51 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import {Container} from "react-bootstrap";
 import HomeScreen from "./screens/homeScreen/homeScreen";
+import {BrowserRouter as Router,Route,Switch,Redirect} from "react-router-dom";
 
 import './_app.scss'
+import LoginScreen from "./screens/loginScreen/loginScreen";
 
-const  App = () =>  {
 
+
+const Layout = ({children}) => {
     const [sidebar,setSidebar] = useState(false);
 
     const handleToggleSidebar = () => {
@@ -15,22 +18,48 @@ const  App = () =>  {
             !prevState
         )
     }
-
-    return (
+    return  (
         <>
-         <Header handleToggleSidebar={ handleToggleSidebar } />
-           <div className="app__container   ">
-               <Sidebar
-                   sidebar={ sidebar }
-                   handleToggleSidebar={ handleToggleSidebar }
-               />
-               <Container fluid className="app_main  ">
-                  <HomeScreen />
-               </Container>
+            <Header handleToggleSidebar={ handleToggleSidebar } />
+            <div className="app__container   ">
+                <Sidebar
+                    sidebar={ sidebar }
+                    handleToggleSidebar={ handleToggleSidebar }
+                />
+                <Container fluid className="app_main  ">
+                    {children}
+                </Container>
 
-           </div>
+            </div>
         </>
-    );
+    )
+}
+
+const  App = () =>  {
+    return  <Router>
+
+        <Switch>
+        <Route path="/" exact >
+            <Layout>
+                <HomeScreen />
+            </Layout>
+        </Route>
+
+        <Route path="/auth" exact>
+             <LoginScreen />
+        </Route>
+
+        <Route path="/search" exact>
+            <Layout>
+                <h1>Search Result</h1>
+            </Layout>
+        </Route>
+
+            <Route>
+                <Redirect to="/" />
+            </Route>
+        </Switch>
+    </Router>
 }
 
 export default App;
