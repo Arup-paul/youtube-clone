5fe7fe42074d8787f176a5a93f2e78eacb21655e
Arup paul <aruppaulctg@gmail.com>
2021-06-08 16:09:39 +0600

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBvWudsC9Zyew8vmblyJUUphS1h3AEnB0",
+    authDomain: "clone-by-arup.firebaseapp.com",
+    projectId: "clone-by-arup",
+    storageBucket: "clone-by-arup.appspot.com",
+    messagingSenderId: "266645166000",
+    appId: "1:266645166000:web:5c357ae327d5e6b5ee6875"
+};
+
+firebase.initializeApp(firebaseConfig)
